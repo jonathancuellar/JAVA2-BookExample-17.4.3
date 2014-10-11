abdc1604ea708dac776b129ed2542d0ac9738567
jonathancuellar <jonathan.cuellar@mavs.uta.edu>
2014-10-10 23:54:20 -0500

    first wave

---
+++ MenuOption.java
@@ -0,0 +1,25 @@
+// Fig 17.11 MenuOption.java
+// Enumeration for the credit inquiry program's options
+
+public enum MenuOption
+{
+    // declare contents of enum type
+    ZERO_BALANCE(1),
+    CREDIT_BALANCE(2),
+    DEBIT_BALANCE(3),
+    END(4);
+
+    private final int value; // current menu option
+
+    // constructor
+    MenuOption(int valueOption)
+    {
+      value = valueOption;
+    } // end menuOptions enum conxtrutor
+
+    // return the value of a constant
+    public int getValue()
+    {
+      return value;
+    } // end method getValue
+} // end enum MenuOption
